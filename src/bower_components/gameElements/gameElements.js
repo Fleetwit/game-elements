1e5976115fe52a129d6ec247945e7af62abe7cba
Julien <julien@twenty-six-medias.com>
2014-01-21 03:30:30 -0200

    New events on keyboards & letter groups

--- src/bower_components/gameElements/gameElements.js
+++ src/bower_components/gameElements/gameElements.js
@@ -12,6 +12,13 @@
 	
 	var gameElements = {
 		letterGroup:	function(container, options) {
+			options = _.extend({
+				gravity:	false,
+				onClick:	function() {
+					
+				}
+			},options);
+			
 			var output = {
 				letters:	[]
 			};
@@ -36,16 +43,39 @@
 						(function(i) {
 							var li = dom("li", ul);
 								li.html(options.letters[i].toUpperCase());
+								li.click(function() {
+									if (!$(this).hasClass("inset")) {
+										options.onClick(i, options.letters[i].toLowerCase(), li);
+										if (options.gravity) {
+											output.gravityStep();
+										}
+									}
+								});
 							output.letters.push(li);
 						})(i);
 					}
 				}
 			}
+			output.gravityStep = function() {
+				for (i=1;i<output.letters.length;i++) {
+					// If the current letter is filled and previous one is empty...
+					if (!output.letters[i].hasClass("inset") && output.letters[i-1].hasClass("inset")) {
+						output.set(i-1, output.letters[i].text());
+						output.empty(i);
+					}
+				}
+			}
 			output.set = function(pos, letter) {
 				output.letters[pos].removeClass("inset").html(letter.toUpperCase());
+				return output;
+			}
+			output.empty = function(pos) {
+				output.letters[pos].removeClass("wrong").addClass("inset").empty();
+				return output;
 			}
 			output.wrong = function(pos) {
 				output.letters[pos].removeClass("inset").addClass("wrong");
+				return output;
 			}
 			output.build();
 			
@@ -107,11 +137,28 @@
 				}
 			}
 			output.disable = function(letters) {
-				letters = letters.split('');
-				_.each(letters, function(letter) {
-					index[letter].removeClass("inset").removeClass("wrong").removeClass("disabled");
-					index[letter].addClass("disabled");
-				});
+				if (!letters || letters === true) {
+					var group = $();
+					for (i in index) {
+						group = group.add(index[i]);
+					}
+					if (letters === true) {
+						group.removeClass("inset").addClass("disabled");
+					} else {
+						group.each(function(idx, element) {
+							element = $(element);
+							if (!element.hasClass("inset")) {
+								element.addClass("disabled");
+							}
+						});
+					}
+				} else {
+					letters = letters.split('');
+					_.each(letters, function(letter) {
+						index[letter].removeClass("inset").removeClass("wrong").removeClass("disabled");
+						index[letter].addClass("disabled");
+					});
+				}
 			}
 			output.inset = function(letters) {
 				letters = letters.split('');
